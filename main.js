cba5b2423c9bb19f80e64b638313fd7811db0c97
WilliamOConnell <williamoconnell00@gmail.com>
2020-02-24 13:53:37 -0500

    add filters

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const domSearchButton = document.getElementById('searchButton');
 const domErrorMessage = document.getElementById('errorMessage');
+const domResultsContainer = document.getElementById('resultsContain');
+const domFilterContainer = document.getElementById('filterContainer');
+const domFilterHead = document.getElementById('filterHead');
 const domResults = document.getElementById('results');
 
 const templateResult = document.getElementById('resultTemplate');
@@ -11,8 +14,12 @@ domSearchButton.addEventListener('click', function() {
         domErrorMessage.innerText = 'Sorry, your browser does not support geolocation.';
     } else {
         domSearchButton.classList.add('loading');
-        navigator.geolocation.getCurrentPosition(gotLocation, failedLocation);
+        navigator.geolocation.getCurrentPosition(gotLocation, failedLocation, {timeout:3000});
     }
+});
+
+domFilterHead.addEventListener('click', function() {
+    domFilterContainer.classList.toggle('open');
 });
 
 function gotLocation(pos) {
@@ -25,20 +32,23 @@ function gotLocation(pos) {
 
 function failedLocation() {
     domSearchButton.classList.remove('loading');
-    domErrorMessage.innerText = 'You must allow geolocation so we can find local results.';
+    domErrorMessage.innerText = 'Unable to get your location.';
 }
 
 async function search() {
     let searchResults = await fetch('/api/search?lat='+geo['lat']+'&lon='+geo['lon']).then(result => result.json());
+    domResults.innerHTML = '';
     searchResults.forEach(result => {
         let root = templateResult.content.cloneNode(true);
         let domItem = root.querySelector('.item');
         domItem.querySelector('.itemImg').src = result['image_url'];
         domItem.querySelector('.location').innerText = result['location']['city'];
         domItem.querySelector('.stars').src = 'img/stars/'+result['rating'].toString().replace('.','_')+'.png';
+        domItem.querySelector('.reviewNumber').innerText = result['review_count'].toString()+' ratings';
         domItem.querySelector('.goButton').href = 'https://www.google.com/maps/dir/?api=1&destination='+result['coordinates']['latitude']+','+result['coordinates']['longitude'];
         domResults.appendChild(domItem);
     });
     console.log(searchResults);
     domSearchButton.classList.remove('loading');
+    domResultsContainer.classList.remove('hidden')
 }
