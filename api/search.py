0e212085e1e335d53761d0e8a248bd9fea14cf33
WilliamOConnell <williamoconnell00@gmail.com>
2020-02-24 10:37:18 -0500

    initial commit

---
+++ api/search.py
@@ -0,0 +1,28 @@
+from flask import Flask, Response, request
+import requests
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def search():
+	if not ('lat' in request.args and 'lon' in request.args):
+		return "Please provide location", 400
+	try:
+		lat = float(request.args['lat'])
+		lon = float(request.args['lon'])
+	except ValueError:
+		return "Invalid lat/lon", 400
+
+	response = requests.get(
+		'https://api.yelp.com/v3/businesses/search',
+		params = {
+			'term': '"Chili\'s"',
+			'latitude': str(lat),
+			'longitude': str(lon)
+		},
+		headers = {
+			'Authorization':'Bearer {}'.format(os.environ['YELP_TOKEN'])
+		}
+	)
+	return Response(response.text, mimetype="application/json")
